3b7cdd00ad01bf9fba234ca7f3fff8d2d167b0c3
Bibas Rai <130794180+bibasrairockz@users.noreply.github.com>
2024-09-18 23:06:19 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 import pandas as pd
 import numpy as np
 
-data = {
-    'applicant_income': np.random.randint(2000, 10000, 100),
-    'loan_amount': np.random.randint(5000, 50000, 100),
-    'credit_history': np.random.choice([0, 1], size=100),
-    'loan_approved': np.random.choice([0, 1], size=100)
-}
+file_path= r'C:\Users\bibas\Downloads\GenAI\pytorch\tarabut\loan_approval_dataset.csv'
 
-loan_df = pd.DataFrame(data)
+loan_data = pd.read_csv(file_path)
 
-print(loan_df.head())
+print(loan_data.head())
